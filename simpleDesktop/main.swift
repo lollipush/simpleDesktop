ed9689c93b6c81dbf0a05f27fdebad708e6481d5
monstersb <naocanmonster@gmail.com>
2015-06-26 23:43:12 +0800

    Initial Commit

---
+++ simpleDesktop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  simpleDesktop
+//
+//  Created by Changzhi Li on 6/26/15.
+//  Copyright (c) 2015 monster. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
