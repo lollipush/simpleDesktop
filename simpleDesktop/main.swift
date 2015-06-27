225b67e40b6af3a803248b25eb8d305e1907d195
monstersb <naocanmonster@gmail.com>
2015-06-28 00:46:13 +0800

    add class application

--- simpleDesktop/main.swift
+++ simpleDesktop/main.swift
@@ -9,9 +9,13 @@
 import Foundation
 import AppKit
 
-println("Hello, World!")
 
-
+class Debug: NSObject {
+    var _debug = false
+    func log(s: String) {
+        println(s)
+    }
+}
 
 class Http: NSObject {
     static func get(surl: String) -> NSData {
@@ -28,22 +32,37 @@ class Http: NSObject {
         return NSData()
     }
     
-    static func download(surl: String, path: String) -> Bool {
+    static func download(surl: String, path: String, fileName: String) -> Bool {
         let data = get(surl)
-        return true
+        let mgr = NSFileManager.defaultManager()
+        mgr.changeCurrentDirectoryPath(path)
+        return mgr.createFileAtPath(fileName, contents: data, attributes: nil)
     }
 }
 
 class ImageListReader: NSObject, NSXMLParserDelegate {
+    var list = [String]()
     override init() {
         super.init()
-        
+    }
+    
+    func getLocalImage() {
+        let mgr = NSFileManager.defaultManager()
+        let files = mgr.contentsOfDirectoryAtPath("./image", error: nil)
+        for i in files! {
+            list.append((i as! String))
+        }
+    }
+    
+    func parseOnline() -> Bool {
         let htmlData = Http.get("http://simpledesktops.com/")
         var err:NSError?
         let xml = NSXMLDocument(data: htmlData, options: 1024, error: &err)
         let dom = NSXMLParser(data: xml!.XMLData)
         dom.delegate = self
-        println(dom.parse())    }
+        return dom.parse()
+    }
+    
     
     func parserDidStartDocument(parser: NSXMLParser) {
         //println("parserDidStartDocument")
@@ -62,10 +81,15 @@ class ImageListReader: NSObject, NSXMLParserDelegate {
         if elementName == "img" {
             var img = (attributeDict["src"]! as! String)
             //println(img)
-            let range = img.rangeOfString(".png");
+            var range = img.rangeOfString(".png");
             //println(range)
             if range != nil {
-                println(img.substringToIndex(range!.endIndex))
+                let imgSrc = img.substringToIndex(range!.endIndex)
+                range = imgSrc.rangeOfString("/", options: NSStringCompareOptions.BackwardsSearch)
+                let fname = imgSrc.substringFromIndex(range!.endIndex)
+                Http.download(imgSrc, path: Application.imageDir, fileName: fname)
+                println(fname)
+                list.append(fname)
             }
         }
     }
@@ -76,15 +100,52 @@ class ImageListReader: NSObject, NSXMLParserDelegate {
 }
 
 class DesktopImageManager: NSObject {
-    func setImage(surl:String) -> Bool {
-        let mgr = NSWorkspace.sharedWorkspace()
+    static func setImage(var surl:String) -> Bool {
+        if surl.substringWithRange(Range(start:  advance(surl.startIndex, 0), end: advance(surl.startIndex, 1))) != "/" {
+            let mgr = NSFileManager.defaultManager()
+            surl = mgr.currentDirectoryPath + "/" + surl
+        }
+        let workspace = NSWorkspace.sharedWorkspace()
         let scr = NSScreen.mainScreen()
-        var opts = mgr.desktopImageOptionsForScreen(scr!)
+        var opts = workspace.desktopImageOptionsForScreen(scr!)
         var err:NSError?
         let url = NSURL(fileURLWithPath: surl)
-        return mgr.setDesktopImageURL(url!, forScreen: scr!, options: nil, error: &err)
+        println(url)
+        return workspace.setDesktopImageURL(url!, forScreen: scr!, options: nil, error: &err)
     }
 }
 
-//ImageListReader()
-DesktopImageManager().setImage("/Users/monster/Desktop/soaring_mountains.png");
+class Application: NSObject {
+    static let lockFile = "./.lock"
+    static let imageDir = "./image"
+    static let interval = 5;
+    let imageList: ImageListReader = ImageListReader()
+    
+    override init() {
+        super.init()
+    }
+    
+    func run () -> Int {
+        _init()
+        imageList.getLocalImage()
+        while (true) {
+            var imgPath = imageList.list[Int(arc4random()) % imageList.list.count]
+            println("wocao " + imgPath)
+            DesktopImageManager.setImage(Application.imageDir + "/" + imgPath)
+            sleep(Application.interval)
+        }
+        return 0
+    }
+    
+    func _init() {
+        let mgr = NSFileManager.defaultManager()
+        if mgr.fileExistsAtPath(Application.lockFile) {
+            return
+        }
+        mgr.createDirectoryAtPath(Application.imageDir, withIntermediateDirectories: true, attributes: nil, error: nil)
+        mgr.createFileAtPath(Application.lockFile, contents: nil, attributes: nil)
+        imageList.parseOnline()
+    }
+}
+
+Application().run()
